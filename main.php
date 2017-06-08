682e58acb4261030175852585fe5e113d45af5a5
Damien Tardy-Panis <damien@m2mobi.com>
2017-06-08 13:13:14 +0200

    add simple main script

---
+++ main.php
@@ -0,0 +1,38 @@
+<?php
+
+include 'bucket.php';
+include 'report.php';
+
+$TMP_CSV       = '/tmp/aws_cost_usage.csv';
+$FUNCTION      = 'output_line_influxdb';
+$BUCKET_PATH   = '/mnt/s3';
+$REPORT_PREFIX = 'hourly';
+$REPORT_NAME   = 'hourly_report';
+
+$manifest_path = get_last_manifest_path(
+    $BUCKET_PATH,
+    $REPORT_PREFIX,
+    $REPORT_NAME
+);
+
+if ($manifest_path === FALSE) {
+    exit;
+}
+
+$reports_paths = get_reports_paths(
+    $BUCKET_PATH,
+    $manifest_path
+);
+
+if ($reports_paths === FALSE) {
+    exit;
+}
+
+foreach ($reports_paths as $report_path) {
+    extract_gzip_report($report_path, $TMP_CSV);
+    parse_report($TMP_CSV, $FUNCTION);
+};
+
+unlink($TMP_CSV);
+
+?>

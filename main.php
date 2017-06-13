94ebc94f8603891e153f0c094d6330b819e38fba
Damien Tardy-Panis <damien@m2mobi.com>
2017-06-13 13:17:42 +0200

    add CLI argument to specify config file to load

--- main.php
+++ main.php
@@ -1,8 +1,23 @@
 <?php
 
+$options = getopt('c:');
+
+if (! isset($options['c'])) {
+    error_log('Missing config file argument "-c file_path"');
+    exit;
+}
+
+$config_file = $options['c'];
+
+if (! file_exists($config_file)) {
+    error_log("Invalid config file path $config_file");
+    exit;
+}
+
+include $config_file;
+
 include 'lib/bucket.php';
 include 'lib/report.php';
-include 'config.php';
 
 $manifest_path = get_current_manifest_path(
     $BUCKET_PATH,
